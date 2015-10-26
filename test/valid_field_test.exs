4b37e2ce9c9ed80498a4408238bbd904e5ae91d3
Dan McClain <git@danmcclain.net>
2015-10-26 10:05:39 -0400

    Add `with_changeset`
    `with_changeset` allows user to provide the changeset as a function to
    be called at testing time

--- test/valid_field_test.exs
+++ test/valid_field_test.exs
@@ -4,33 +4,43 @@ defmodule ValidFieldTest do
   doctest ValidField
 
   test "valid field values" do
-    %Model{}
+    ValidField.with_changeset(%Model{})
     |> ValidField.assert_valid_field(:first_name, ["Test", "Good Value"])
     |> ValidField.assert_valid_field(:last_name, ["", nil, "Something"])
     |> ValidField.assert_valid_field(:title, ["", nil, "Something else"])
 
     assert_raise ExUnit.AssertionError, "Expected the following values to be invalid for \"first_name\": \"Test\", \"Good Value\"", fn ->
-      %Model{}
+      ValidField.with_changeset(%Model{})
       |> ValidField.assert_invalid_field(:first_name, ["Test", "Good Value"])
     end
 
     assert_raise ExUnit.AssertionError, "Expected the following values to be invalid for \"last_name\": \"\", nil, \"Something\"", fn ->
-      %Model{}
+      ValidField.with_changeset(%Model{})
       |> ValidField.assert_invalid_field(:last_name, ["", nil, "Something"])
     end
 
     assert_raise ExUnit.AssertionError, "Expected the following values to be invalid for \"title\": \"\", nil, \"Something else\"", fn ->
-      %Model{}
+      ValidField.with_changeset(%Model{})
       |> ValidField.assert_invalid_field(:title, ["", nil, "Something else"])
     end
   end
 
   test "invalid field values" do
-    %Model{}
+    ValidField.with_changeset(%Model{})
     |> ValidField.assert_invalid_field(:first_name, ["", nil])
 
     assert_raise ExUnit.AssertionError, "Expected the following values to be valid for \"first_name\": \"\", nil", fn ->
-      %Model{}
+      ValidField.with_changeset(%Model{})
+      |> ValidField.assert_valid_field(:first_name, ["", nil])
+    end
+  end
+
+  test "passing funciton to changeset" do
+    custom_changeset_function = ValidField.with_changeset(%Model{}, &Model.changeset/2)
+    |> ValidField.assert_invalid_field(:first_name, ["", nil])
+
+    assert_raise ExUnit.AssertionError, "Expected the following values to be valid for \"first_name\": \"\", nil", fn ->
+      custom_changeset_function
       |> ValidField.assert_valid_field(:first_name, ["", nil])
     end
   end
