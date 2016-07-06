7ba1b703dafcce8fe4b8f1f82f1d58c410a6e557
Eugene Pirogov <iamexile@gmail.com>
2016-07-06 21:14:35 +0300

    Introduce ValidationException

---
+++ lib/valid_field/validation_exception.ex
@@ -0,0 +1,21 @@
+defmodule ValidField.ValidationException do
+  @moduledoc """
+  Raised my exception.
+  """
+
+  defexception [:field, :values, :validity]
+
+  def message(exception) do
+    formatted_field = inspect(Atom.to_string(exception.field))
+    formatted_values = _format_values(exception.values)
+    validity = exception.validity
+
+    "Expected the following values to be #{validity} for #{formatted_field}: #{formatted_values}"
+  end
+
+  defp _format_values(values) do
+    values
+    |> Enum.map(&inspect/1)
+    |> Enum.join(", ")
+  end
+end
